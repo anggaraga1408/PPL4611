400d91cde8c84e3ba842a3d8a795d9826b482f4a
Maheswara Athallah Wijanarko <maheswara.atha.w@gmail.com>
2021-04-06 10:54:31 +0700

    Update DashboardAdmin.java

--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/DashboardAdmin.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/DashboardAdmin.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.catalogboardgame.R;
+
 public class DashboardAdmin extends AppCompatActivity {
 
     @Override
